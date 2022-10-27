f230104c9b75a516ed0236ee4a643a70d4954f18
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-10-26 19:21:45 -0700

    Use jquery to display current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,4 @@
-
 //get current day info
 var today = moment();
-
-//hook DOM
-var pElcurrentDate = document.querySelector("#currentDay");
-
 // display current date
-pElcurrentDate.textContent = today.format("dddd, MMMM Do YYYY");
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
