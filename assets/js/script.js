9695be76a3631b83ed534c816d1d8cae960f65c9
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-10-26 19:42:21 -0700

    Add div row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 //get current day info
 var today = moment();
 
+//Hook DOM
+var divContainer = $(".container");
+
 // display current date
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
 // Create work hours array
 var workHours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
 
+var divRow = $("<div>", {"class": "row"});
+divContainer.append(divRow);
 
+$.each(workHours, function(index, el){
+    console.log(el);
+});
