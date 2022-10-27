1707bd4d9de03cfe63585dd1d8022df1be6d84ce
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-10-26 19:27:22 -0700

    Add work hours array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 //get current day info
 var today = moment();
+
 // display current date
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+// Create work hours array
+var workHours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+
+
