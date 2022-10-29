31b482e5b324408fa2569bd2fb8aea6bce9f7fca
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-10-29 11:29:54 -0700

    Add click handler to change saveBtn color and show message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,48 @@
 //get current day info
 var today = moment();
 var colorTime="";
+
+//array of events
+var tasksArr = [];
+
 //Hook DOM
 var $divContainer = $(".container");
 
 // display current date
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
-// Create work hours array
-var workHours = ["12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM",
-"11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"];
-
 //curent time variable (formatted am/pm)
 var currentTime = (moment().format("hA"));
 
+// Create work hours array
+var workHours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM"];
+
+//index of current time element from the workHours array
 var indexOfCurrentTime = workHours.indexOf(currentTime);
+
+//create element to display save message
+var $saveMessage = $("<h6>",{"class": "text-center d-none"});
+$saveMessage.html("Task saved to <span class='text-success'>local storge </span><span class='text-warning'>&#10003</span>");
+$divContainer.append($saveMessage);
 
 //generate content for div container
 $.each(workHours, function(index, hour){
     colorTime = (index<indexOfCurrentTime)?"past":(index>indexOfCurrentTime)?"future":"present";
     var $divRow = $("<div>", {"class": "row time-block"});
     $divRow.append($("<div>",{"class": "hour col-1 text-right pr-0 d-flex justify-content-end align-items-center"}).text(hour));
-    $divRow.append($("<textarea>",{"class": "col-10 "+colorTime+" text-left description"}));
+    $divRow.append($("<textarea>",{"class": "col-10 "+colorTime+" text-left description", "id" : index}).val(""));
     $divRow.append($("<div>",{"class": "col-1 align-item-center d-flex justify-content-center align-items-center saveBtn"})
     .append($("<i>", {"class": "fa fa-save my-4"})));
     $divContainer.append($divRow);
+});
+
+// change save icon color, show message by click and save to local storage
+$("i").click(function(ev){
+    ev.currentTarget.style.color = "black";
+    $saveMessage.addClass("d-block");
+    setTimeout(function() {
+        ev.currentTarget.style.color = "white"; 
+        $saveMessage.removeClass("d-block");
+      }, 400)
+        
 });
